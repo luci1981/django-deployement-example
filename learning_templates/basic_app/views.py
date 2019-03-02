a86154d8c2b5f0f8b4146231517f984b6c7b71bc
luci1981 <mrluci1981@gmail.com>
2019-03-02 11:20:12 +0200

    Update views.py

--- learning_templates/basic_app/views.py
+++ learning_templates/basic_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
     context_dict = {'text': 'hello world', 'number':100}
     return render(request, 'basic_app/index.html', context_dict)
